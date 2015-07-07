0b0daa860db7c2dc747a57babe9b5248323d8351
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2015-07-07 23:21:21 +0300

    Created bg-accounts package for managing accounts Added accounts ui-package for start Added UI template with loginButtons inside Added UI template to header

---
+++ packages/bg-accounts/config.js
@@ -0,0 +1,15 @@
+/*
+  configuring accounts and login services
+  all services ids and secrets stored in private/services_settings.json
+*/
+
+var configs = JSON.parse(Assets.getText("private/services_settings.json"));
+
+//accounts
+
+//facebook
+ServiceConfiguration.configurations.upsert({service: 'facebook'}, {
+  $set: configs.facebook
+});
+
+//google
---
+++ packages/bg-accounts/package.js
@@ -0,0 +1,39 @@
+Package.describe({
+  summary: "Accounting package for board-gamer",
+  version: "0.0.1",
+  name: "simterface:bg-accounts"
+});
+
+Package.onUse(function(api) {
+  var client = 'client', server = 'server', both = [client, server];
+  //packages
+  api.use([
+      'accounts-base',
+      'accounts-password',
+      'accounts-facebook',
+      'accounts-ui',
+      'service-configuration'
+  ], both);
+
+  api.use([
+      'templating'
+    ], client);
+
+  //files
+  //shared
+  // api.addFiles([], both);
+
+  //server only
+  api.addFiles([
+      'config.js'
+    ], server);
+  //Assets
+  api.addFiles([
+    'private/services_settings.json'
+  ],server,{isAsset: true});
+
+  //client only
+  api.addFiles([
+      'bg_accounts_ui.html'
+    ], client);
+});
